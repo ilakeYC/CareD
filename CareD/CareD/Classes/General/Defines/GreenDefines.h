880e67d8c215320d66927ce530685c05a11a9859
LakesMac <15076610315@sina.cn>
2015-11-09 11:22:16 +0800

    1511091121-YC-PCH文件引入组员Headers，负责引入你们的头文件

---
+++ CareD/CareD/Classes/General/Defines/GreenDefines.h
@@ -0,0 +1,13 @@
+//
+//  GreenDefines.h
+//  CareD
+//
+//  Created by LakesMac on 15/11/9.
+//  Copyright © 2015年 Tec-Erica. All rights reserved.
+//
+
+#ifndef GreenDefines_h
+#define GreenDefines_h
+
+
+#endif /* GreenDefines_h */
---
+++ CareD/CareD/Classes/General/Defines/HYDefines.h
@@ -0,0 +1,13 @@
+//
+//  HYDefines.h
+//  CareD
+//
+//  Created by LakesMac on 15/11/9.
+//  Copyright © 2015年 Tec-Erica. All rights reserved.
+//
+
+#ifndef HYDefines_h
+#define HYDefines_h
+
+
+#endif /* HYDefines_h */
